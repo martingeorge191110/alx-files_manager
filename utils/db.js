d6c3534db8d5102972b7bcd228761ce245faaf27
Martin George <martingeorge191110@gmail.com>
2025-06-21 14:53:06 +0300

    Task 1 (Data base)

---
+++ utils/db.js
@@ -0,0 +1,40 @@
+import { MongoClient } from 'mongodb';
+
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${host}:${port}`;
+
+class DBClient {
+  constructor() {
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.db = null;
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(database);
+      })
+      .catch((err) => {
+        this.db = null;
+        console.log('MongoDB Client Error', err);
+      });
+  }
+
+  isAlive = () => {
+    return (!!this.db);
+  }
+
+  nbFiles = () => {
+   if (!this.db) return (0);
+   return (this.db.collection('files').countDocuments());
+ }
+
+  nbUsers = async () => {
+    if (!this.db) return 0;
+    return (this.db.collection('users').countDocuments());
+  }
+
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
